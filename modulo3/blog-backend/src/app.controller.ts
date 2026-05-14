72c3df8b85ed346a0277e6a72dd143f5a500df3e
Karlamos <mosqueraalmeida2006@gmail.com>
2026-05-14 03:59:00 -0500

    clase 15/05

--- modulo3/blog-backend/src/app.controller.ts
+++ modulo3/blog-backend/src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ProductoDto } from './product.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/health")
+  getHeath(): any {
+    return this.appService.getHeath();
+  }
+
+  @Post("/products")
+  createProduct(@Body() product: ProductoDto): ProductoDto {
+    return this.appService.createProduct(product);
+  }
+
+  @Get("/products")
+  findAll(): ProductoDto[] {
+    return this.appService.findAll();
+  }
+
+  @Get("/products/:id")
+  findById(@Param('id') id: string): ProductoDto {
+    return this.appService.findById(id);
   }
 }
--- modulo3/blog-backend/src/app.service.ts
+++ modulo3/blog-backend/src/app.service.ts
@@ -1,8 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { ProductoDto } from './product.dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private products: ProductoDto[] = [
+    {
+      id: 1,
+      name: "Laptop",
+      price: 850,
+      stock: 10
+    },
+
+    {
+      id: 2,
+      name: "Mouse",
+      price: 25,
+      stock: 50
+    },
+  ];
+
+  getHeath(): any {
+    return {
+      "status": "Online",
+      "service": "blog service api",
+      "version": "0.0.1",
+      "date": new Date()
+    };
+  }
+
+  createProduct(product: ProductoDto): ProductoDto {
+    const newProduct: ProductoDto = {
+      id: Math.floor(Math.random() * 1000)+1,
+      ...product
+    }
+    this.products.push(newProduct);
+    return {
+      "id": newProduct.id,
+      "name": newProduct.name,
+      "price": newProduct.price,
+      "stock": newProduct.stock
+    };
+  }
+
+  findAll(): ProductoDto[] {
+    return this.products;
+  }
+
+  findById(id: string): ProductoDto {
+    return this.products!
+      .find(product=> product.id === Number(id))!;
   }
 }
