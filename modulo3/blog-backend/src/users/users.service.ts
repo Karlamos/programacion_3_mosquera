29752d4eeb0b520c02976af4ec59ba0c576af00b
Karlamos <mosqueraalmeida2006@gmail.com>
2026-05-28 04:01:47 -0500

    clase 05/28

--- modulo3/blog-backend/src/users/users.service.ts
+++ modulo3/blog-backend/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -6,14 +6,14 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import * as bcrypt from 'bcrypt';
 import { IPaginationOptions, paginate, Pagination } from 'nestjs-typeorm-paginate';
-import { QueryDto } from 'src/common/dto/query.dto';
+import { QueryDto } from '../common/dto/query.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-  ) {}
+  ) { }
 
   async create(createUserDto: CreateUserDto): Promise<User | null> {
     try {
@@ -108,5 +108,12 @@ export class UsersService {
     const user = await this.userRepository.findOne({ where: { id } });
     if (!user) return null;
     return this.userRepository.remove(user);
+  }
+
+  async updateProfile(id: string, profile: string) {
+    const user = await this.userRepository.findOne({ where: { id: id } });
+    if (!user) throw new NotFoundException('User not found');
+    user.profile = profile;
+    return this.userRepository.save(user);
   }
 }
