75e13cd331f23697810359b323bcfc14840bf044
Karlamos <mosqueraalmeida2006@gmail.com>
2026-06-01 03:58:51 -0500

    Clase 01/06

--- modulo3/blog-backend/src/mail/mail.controller.ts
+++ modulo3/blog-backend/src/mail/mail.controller.ts
@@ -12,4 +12,10 @@ export class MailController {
     const result = await this.mailService.sendMail(dto);
     return new SuccessResponseDto('Correo enviado con Gmail', result);
   }
+
+  @Get('public-api')
+  async getUsersFromPublicApi() {
+  const result = await this.mailService.fetchUserListFromPublicApi();
+  return new SuccessResponseDto('Usuarios obtenidos', result);
+  }
 }
